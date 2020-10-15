8a7d3313494a6365aacf0da72a85ff15b5e3c592
KirthikVaishnav <69428343+KirthikVaishnav@users.noreply.github.com>
2020-10-15 16:12:53 +0530

    Update bubble.java

--- Sort/BubbleSort/bubble.java
+++ Sort/BubbleSort/bubble.java
@@ -1,40 +1,33 @@
 import java.util.Scanner;
-
-public class bubble {
+public class bubblesort {
     public static void bubsort(int[] a)
-    {
-        for (int i = 0; i <a.length ; i++) {
-            for (int j = i+1; j <a.length ; j++) {
-                if(a[j]<a[j+1])
+    {    int n=a.length;
+        for (int i = 0; i <n-1; i++) {
+            for(int j=i+1;j<n-i-1;j++)
+            {
+                if(a[i]>a[j])     //Comparision Operator
                 {
-                    int temp=a[j];
-                    a[j]=a[j+1];
-                    a[j+1]=a[j];
+                    int temp=a[i];
+                    a[i]=a[j];
+                    a[j]=temp;
                 }
             }
-            System.out.println("Sort after i="+ i+1);
             printarr(a);
+            System.out.println("i="+i);  //Printing array after each step
         }
     }
-    public static void printarr(int[] a)
+    public static void printarr(int[] arr)
     {
-        for (int i = 0; i <a.length ; i++) {
-            System.out.println(a[i]);
+        for (int i = 0; i <arr.length; i++) {
+            System.out.println(arr[i]+"  ");
         }
     }
     public static void main(String args[])
     {
         Scanner obj=new Scanner(System.in);
-        int[] a=new int[20];
-        int n;
-        System.out.println("Enter array size:");
-        n=obj.nextInt();
-        System.out.println("Enter array elements");
-        for (int i = 0; i <n; i++)
-        {
-            a[i]=obj.nextInt();
-        }
-        bubsort(a);
-        printarr(a);
+        int[] a={2,3,1,5,4}; //Input Array
+        bubsort(a);    //Passing the array to the sorting function
+        System.out.println("Sorted Array:");
+        printarr(a);    //Printing Sorted array
     }
 }
