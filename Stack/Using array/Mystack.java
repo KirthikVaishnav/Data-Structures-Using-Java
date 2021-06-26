b5e68c08c066410261e3b3df19886e3cb740ec55
KirthikVaishnav <69428343+KirthikVaishnav@users.noreply.github.com>
2021-06-26 18:58:28 +0530

    Create Mystack.java

---
+++ Stack/Using array/Mystack.java
@@ -0,0 +1,29 @@
+public class Mystack{
+    int arr[]=new int[5];
+    int top=0;
+    public void push(int data)
+    {
+        arr[top]=data;
+        top++;
+    }
+    public int pop()
+    {
+        int data;
+        top--;
+        data=arr[top];
+        arr[top]=0;
+        return data;
+    }
+    public int peek()
+    {
+        int data;
+        data=arr[top-1];
+        return data;
+    }
+    public void show()
+    {
+        for (int i=0;i<arr.length;i++) {
+            System.out.println(arr[i]+" ");
+        }
+    }
+}
