3f258471f6c52b33d1b14884d3fe6c66d102d91a
AD <KAD@ford.com>
2023-08-14 10:18:23 +0530

    GIT CONNECTED

--- Problems on Strings/finalvalueofvariable.java
+++ Problems on Strings/finalvalueofvariable.java
@@ -4,6 +4,7 @@ class Solution {
         for(int i=0;i<operations.length;i++)
         {
             String s =operations[i];
+            System.out.println("GIT IS UPDATED");
             if(s.charAt(0)=='X' && s.charAt(1)=='+')
                 ans++;
              else if(s.charAt(0)=='X'&&s.charAt(1)=='-')
