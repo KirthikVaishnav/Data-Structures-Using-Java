820a88f5fc5f0d6298b2c9a7e3e8a5f61e6b6fad
KirthikVaishnav <69428343+KirthikVaishnav@users.noreply.github.com>
2023-10-11 14:49:19 +0530

    Delete Application Of Array directory

--- Application Of Array/arrayfrompermutation.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public int[] buildArray(int[] nums) {
-        int[] arr = new int[nums.length];
-  for(int i = 0; i < arr.length; i++){
-      arr[i] = nums[nums[i]];
-  }
-  return arr;
-    }
-}
