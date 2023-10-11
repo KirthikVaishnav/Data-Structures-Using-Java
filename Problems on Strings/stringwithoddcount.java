cb1041c1fbd17dd7a6ac0e8611a3dfb6aa658bc6
KirthikVaishnav <69428343+KirthikVaishnav@users.noreply.github.com>
2023-10-11 14:48:16 +0530

    Delete Problems on Strings directory

--- Problems on Strings/stringwithoddcount.java
+++
@@ -1,8 +0,0 @@
-class Solution {
-    public String generateTheString(int n) {
-        char[] result = new char[n];
-        Arrays.fill(result,'a');
-        if(n%2 == 0)result[0] = 'b';
-        return new String(result);
-    }
-}
