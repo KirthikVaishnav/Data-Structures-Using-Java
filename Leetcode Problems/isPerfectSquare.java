5b03d4ce6d90afd1ae389cbfa4ae861671ff41e3
KirthikVaishnav <69428343+KirthikVaishnav@users.noreply.github.com>
2023-10-11 14:48:58 +0530

    Delete Leetcode Problems directory

--- Leetcode Problems/isPerfectSquare.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public boolean isPerfectSquare(int num) {
-        for (int i = 1; i <= 46340; i++)
-        if (i * i == num)
-            return true;
-
-         return false;
-    }
-}
